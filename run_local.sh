7f3d679c6dafe8e2f1c54a74fe6cfe95c8529c48
Jin Pan <jinxp1@gmail.com>
2022-07-21 14:14:18 -0700

    app2: format & update docker run flags with --rm

--- run_local.sh
+++ run_local.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 docker build -t svelte-playground .
 docker run --name svelte_playground \
-    -it -e "TERM=xterm-256color" \
+    --rm -it -e "TERM=xterm-256color" \
     -p "5173:5173" \
     -v "$(pwd)":/src \
     svelte-playground
